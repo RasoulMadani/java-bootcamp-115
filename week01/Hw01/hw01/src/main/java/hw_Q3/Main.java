49083de67ea3939e245fbe49c007a9384926abf5
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-30 14:20:50 +0330

    تمیز کردن کد

--- week01/Hw01/hw01/src/main/java/hw_Q3/Main.java
+++ week01/Hw01/hw01/src/main/java/hw_Q3/Main.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        int  n;
         Scanner intput = new Scanner(System.in);
-
         System.out. println("Enter n:");
-        n = intput.nextInt();
-
+        int n = intput.nextInt();
         for (int  i = 0; i <= n; i++) {
             for (int j = i; j < n; j++)
                 System.out.print("  ");
@@ -20,7 +15,5 @@ public class Main {
             }
             System.out. println("");
         }
-
-
     }
 }
