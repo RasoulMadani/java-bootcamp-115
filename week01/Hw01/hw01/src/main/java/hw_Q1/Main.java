32ef3b122b2a3006f409f41d5a1f1a463823a551
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-30 13:17:32 +0330

    hw01 - q1

---
+++ week01/Hw01/hw01/src/main/java/hw_Q1/Main.java
@@ -0,0 +1,24 @@
+package hw_Q1;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Salary:");
+        int salary = input.nextInt();
+        double tax = 0;
+        if (salary <= 3000000) {
+             tax = (salary * 5) / 100;
+        } else if (salary >= 6000000) {
+             tax = (salary * 24) / 100;
+        } else if (salary < 6000000) {
+             tax = (salary * 10) / 100;
+        }
+        System.out.println(tax + " Toman");
+    }
+}
