55650786c508916b8b5a32578773b4dc58cf0568
Rasoulmadani <ali.rahbary@chmail.ir>
2024-04-27 09:37:38 +0330

    change package

--- week05/day03/cw/src/main/java/q_06/ConvertBase2To10.java
+++ week05/day03/cw/src/main/java/q_06/ConvertBase2To10.java
@@ -1,7 +1,11 @@
-package q_05;
+package q_06;
+
+
+import java.util.Scanner;
 
 public class ConvertBase2To10 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Integer input = scanner.nextInt();
         int digit = 0;
         int baseTeen = 0;
